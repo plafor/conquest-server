9544d9a0bc16ec1a2e5b70b31f3011f66d6e41c6
aytsukii <raptux@gmail.com>
2016-11-28 10:16:52 +0100

    Minor text fixes

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>Documentation</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><a href="https://github.com/CodeOfCinders/conquest-server" target="_blank">Github</a></li>
+      <li><a href="https://www.gitbook.com/book/aytsukii/conquest-server/welcome" target="_blank">Documentation</a></li>
     </ul>
   </div>
 </template>
@@ -25,7 +14,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Bienvenu sur l\'interface admin du jeu conquest'
     }
   }
 }
